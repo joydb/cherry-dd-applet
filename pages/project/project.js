7a757bdf4822faddb7660e87951dd4304c459bda
zhaoyunxing92 <2385585770@qq.com>
2020-05-05 01:10:36 +0800

    up:me icon

--- pages/project/project.js
+++ pages/project/project.js
@@ -3,7 +3,8 @@ Page({
         projects: [
             {
                 name: "反诈平台",
-                desc: "这个就是简单介绍下"
+                desc: "这个就是简单介绍下",
+                createTime:"2020-5-5 00:55:52"
             },
             {
                 name: "反诈平台",
